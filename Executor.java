eedf7da26892dd0d5117e2b2ac012018d5ee84b6
tharani-9080 <tharanis246@gmail.com>
2025-07-17 15:02:30 +0530

    Create Executor.java

---
+++ Executor.java
@@ -0,0 +1,15 @@
+package com.tnsif.oopsconcept;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		
+		EncapsulationDemo ed = new EncapsulationDemo();
+		ed.setName("Rohit Sharma");
+		ed.setAge(37);
+		ed.setJersyNo(45);
+		
+		System.out.println(ed);
+	}
+
+}
