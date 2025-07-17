520d383d3ed3e23d9579d014f08e04dcafdd5aa3
tharani-9080 <tharanis246@gmail.com>
2025-07-17 15:28:20 +0530

    Create myfrist.java

---
+++ myfrist.java
@@ -0,0 +1,16 @@
+package com.tns.junit.testing;
+
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+	
+	
+	@Test //basic testing annotation
+	void display()
+	{
+		System.out.println("Hello World");
+	}
+	
+	
+
+}
